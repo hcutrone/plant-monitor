d9562a4e32f51ffe1938c5c08ef71699fec61be9
HCutrone <hcutrone@calpoly.edu>
2023-03-02 16:00:49 -0800

    Initial Commit

---
+++ Plant Monitor/Plant_MonitorApp.swift
@@ -0,0 +1,17 @@
+//
+//  Plant_MonitorApp.swift
+//  Plant Monitor
+//
+//  Created by Harrison Cutrone on 3/2/23.
+//
+
+import SwiftUI
+
+@main
+struct Plant_MonitorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
